f93a7e2c0bcd8d594a83af4078a12a01ced5bc2d
gidmoth <gidoramothra@gmail.com>
2021-05-06 17:12:54 +0200

    backup  commit

--- src/App.js
+++ src/App.js
@@ -8,48 +8,59 @@ import UserList from './components/UserList';
 
 function App(props) {
 
-  const [xmlState, setXmlState] = useState({
-    "op": "info/state",
-    "state": {
-      "info": {},
-      "globals": {},
-      "users": [],
-      "conferencetypes": [],
-      "conferences": []
-    }
-  })
+  // get control states and setters
+  const [mode, setMode] = useState('startpage')
+  const [initXml, setInitXml] = useState(true)
+  const [xmlState, setXmlState] = useState()
 
+  // effects on states
   useEffect(() => {
-    fetch('https://gsphone.c8h10n4o2.gs/api/info/state', {
-      method: 'GET',
-      credentials: 'include'
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-    })
-  }, [])
+    if (initXml) {
+      fetch('https://gsphone.c8h10n4o2.gs/api/info/state', {
+        method: 'GET',
+        credentials: 'include'
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setXmlState(data)
+          setInitXml(false)
+        })
+    }
+  }, [initXml])
 
-  const {
-    info,
-    globals,
-    users,
-    conferencetypes,
-    conferences
-  } = xmlState.state
+  // variables for xmlState destructuring
+  let info, globals, users, conferencetypes, conferences
 
-  
+  // conditional xmlState destructuring
+  if (xmlState) {
+    ({ info, globals, users, conferencetypes, conferences } = xmlState.state)
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {`fsconcli on ${window.location.origin}`}
-        </p>
-      </header>
-      <UserList list={users} />
-    </div>
-  );
+  // checkbutton handler
+  function handleCheckButton() {
+    setInitXml(true)
+  }
+
+  // render according to state
+  switch  (mode) {
+    case 'startpage': {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              {`fsconcli on ${window.location.origin}`}
+            </p>
+          </header>
+          {users && <UserList list={users} />}
+          <button onClick={handleCheckButton}>reset initXml</button>
+        </div>
+      );
+    }
+    default: {
+      return null
+    }
+  }
 }
 
 export default App;
