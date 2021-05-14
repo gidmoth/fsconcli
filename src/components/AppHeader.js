67a5f584054ad3b0121c23794a7bb37825b80b9f
gidmoth <gidoramothra@gmail.com>
2021-05-14 08:13:20 +0200

    backup

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,116 +3,44 @@
  */
 
 import './AppHeader.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import MPcontainer from './MPcontainer'
-import Icon from '@material-ui/core/Icon'
+
+
+function reducer(state, action) {
+
+    //'expand_less'
+}
 
 
 function AppHeader(props) {
 
     // get control states and setters
-    const [mpstate, setMpstate] = useState({
+    const [mpstate, dispatch] = useReducer(reducer, {
         collapsed: true,
-        phone: false
+        show: 'menu',
+        phoneicn: 'phone_enabled',
+        menuicn: 'menu'
     })
-
-    // effects on states
 
     // destructure props
     const { switchMode, apiorigin } = props
 
-    // mpstate updaters
-    function handleMenuclick() {
-        switch (mpstate.collapsed) {
-            case true: {
-                setMpstate({ collapsed: false, phone: false })
-            }
-            case false: {
-                switch (mpstate.phone) {
-                    case true: {
-                        setMpstate({ collapsed: false, phone: false })
-                    }
-                    case false: {
-                        setMpstate({ collapsed: true, phone: false })
-                    }
-                }
-            }
-        }
-    }
-
-    function handlePhoneClick() {
-        switch (mpstate.collapsed) {
-            case true: {
-                setMpstate({ collapsed: false, phone: true })
-            }
-            case false: {
-                switch (mpstate.phone) {
-                    case true: {
-                        setMpstate({ collapsed: true, phone: true })
-                    }
-                    case false: {
-                        setMpstate({ collapsed: false, phone: true })
-                    }
-                }
-            }
-        }
-    }
-
-    // icon display deciders
-    function meuBtnTxt() {
-        switch (mpstate.collapsed) {
-            case true: {
-                return 'menu'
-            }
-            case false: {
-                switch (mpstate.phone) {
-                    case true: {
-                        return 'menu'
-                    }
-                    case false: {
-                        return 'expand_less'
-                    }
-                }
-            }
-        }
-    }
-
-    function phoneBtnTxt() {
-        switch (mpstate.collapsed) {
-            case true: {
-                return 'phone_enabled'
-            }
-            case false: {
-                switch (mpstate.phone) {
-                    case true: {
-                        return 'expand_less'
-                    }
-                    case false: {
-                        return 'phone_enabled'
-                    }
-                }
-            }
-        }
-    }
-
-    const meubtn = meuBtnTxt()
-    const phonebtn = phoneBtnTxt()
-
     return (
-        <>
+        <div className='headcontainer'>
             <header className="App-header">
-                <span className='meuBtn symb' onClick={handleMenuclick}>
-                    <icon>{meubtn}</icon>
+                <span className='meuBtn symb' onClick={() => dispatch({ type: 'menu' })}>
+                    {mpstate.menuicn}
                 </span>
                 <span className='meuTitle'>
                     {`${apiorigin.split('//')[1].split('.')[0]}`}
                 </span>
-                <span className='phoneBtn symb' onClick={handlePhoneClick}>
-                    <icon>{phonebtn}</icon>
+                <span className='phoneBtn symb' onClick={() => dispatch({ type: 'phone' })}>
+                    {mpstate.phoneicn}
                 </span>
             </header>
             <MPcontainer />
-        </>
+        </div>
     );
 }
 
