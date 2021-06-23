c25be34d2e887ca32c50faa37f54933b85c571fb
gidmoth <gidoramothra@gmail.com>
2021-06-23 15:56:24 +0200

    pausecommit

--- src/components/team/ConfLive.js
+++ src/components/team/ConfLive.js
@@ -6,7 +6,13 @@ import './Users.css';
 import './RegList.css'
 import { useContext, useEffect, useReducer, useRef } from 'react'
 import { LiveContext } from '../LiveContext'
+import { XmlContext } from '../XmlContext'
 import CallBtn from '../CallBtn'
+
+function getConfList(lconfs, xmlconfs) {
+    return lconfs.map(c => ({ ...c, num: xmlconfs.find(k => k.name === c.name).num }))
+}
+
 
 function LiveConf(props) {
 
@@ -18,28 +24,45 @@ function LiveConf(props) {
         lastjoin,
         lastleave,
         memcount,
-        members
+        members,
+        num
     } = props.conf
 
     return (
         <div className={'liveconf'}>
-            <strong>{name}</strong>
-            <dl>
-                <dt>floor:</dt>
-                <dd>{floor.name}</dd>
-                <dt>lastjoin:</dt>
-                <dd>{lastjoin.name}</dd>
-                <dt>lastleave:</dt>
-                <dd>{lastleave.name}</dd>
-                <dt>recording:</dt>
-                <dd>{recording.status}</dd>
-                <dt>locked:</dt>
-                <dd>{JSON.stringify(locked)}</dd>
-                <dt>membercount:</dt>
-                <dd>{memcount}</dd>
-            </dl>
+            <div className='namec'><strong>{name}</strong></div>
+            <div className='callconf'>
+                <CallBtn number={num} />
+            </div>
+            <div className='muteconf ctr symb'>
+                mic_off
+            </div>
+            <div className='lockconf ctr symb'>
+                lock
+            </div>
+            <div className='kickconf ctr symb'>
+                remove_circle
+            </div>
+            <div className='flrusr'>
+                <strong>floor</strong><br />{floor.name}
+            </div>
+            <div className='symb flrctr'>
+                {floor.mute ? 'mic' : 'mic_off'}
+            </div>
+            {/* <div>{JSON.stringify(props.conf)}</div> */}
         </div>
     )
+}
+
+function reducer(currstate, evn) {
+    switch (evn.e) {
+        case 'newlist': {
+            return { ...currstate, conflist: evn.data }
+        }
+        default: {
+            console.log('hit default!')
+        }
+    }
 }
 
 
@@ -49,11 +72,21 @@ function ConfLive() {
     const { liveState } = useContext(LiveContext)
     const { conferences } = liveState
 
+    const { xmlState } = useContext(XmlContext)
+
+    const [state, dispatch] = useReducer(reducer, {
+        conflist: getConfList(conferences, xmlState.conferences),
+    })
+
+    useEffect(() => {
+        dispatch({ e: 'newlist', data: getConfList(conferences, xmlState.conferences) })
+    }, [conferences, xmlState])
+
 
     return (
         <>
             <div className={'LconList'}>
-                {conferences.map(conf => <LiveConf
+                {state.conflist.map(conf => <LiveConf
                     conf={conf}
                     key={conf.name}
                 />)}
